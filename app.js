aee2618ab3843d32dcc467059042058f5c9e8718
John Sanford <johnsanfordstudio@gmail.com>
2015-10-14 15:55:45 -0700

    added chart and made function

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var imgL, imgR;
 
-var Photo = function(name, path) {
-  this.name = name;
+var Photo = function(fileName, path) {
+  this.fileName = fileName;
   this.path = path;
   this.votes = 0;
 }
-
+//object instances that go into array
 var f1 = new Photo('flower1', 'img/flower1.jpg');
 var f2 = new Photo('flower2', 'img/flower2.jpg');
 var f3 = new Photo('flower3', 'img/flower3.jpg');
@@ -29,20 +29,67 @@ tracker.randNum = function() {
 }
 
 tracker.twoImages = function() {
-  imgL = tracker.flow_arr[this.randNum()].path;
-  imgR = tracker.flow_arr[this.randNum()].path;
+  imgL = tracker.flow_arr[this.randNum()];
+  imgR = tracker.flow_arr[this.randNum()];
+	var img1 = document.getElementById('imageL');
+	var img2 = document.getElementById('imageR');
+
+	img1.src = imgL.path;
+	img2.src = imgR.path;
 
   while (imgR === imgL){
   	console.log('dup found');
-     imgR = tracker.flow_arr[this.randNum()].path;
+     imgR = tracker.flow_arr[this.randNum()];
   }
 	console.log(imgL, imgR);
 }
+// console.log("imageL", imgL);
+	tracker.twoImages();
+ 
+var imgLeftButton = document.getElementById('imageL');
+imgLeftButton.addEventListener('click', function() {
+	console.log('imgL ' + imgL.votes);
+	imgL.votes +=1;
+	tracker.twoImages();
+	makeChart();
+	console.log(f1.votes);
+});
 
-var img1 = document.getElementById('imageL');
-var img2 = document.getElementById('imageR');
-tracker.twoImages();
+var imgRightButton = document.getElementById('imageR');
+imgRightButton.addEventListener('click', function() {
+	console.log(imgR.votes);
+	imgR.votes +=1;
+	tracker.twoImages();
+	makeChart();
+	console.log('imgR ' + imgR.votes);
+});
+	   
+var makeChart = function() {
+	var data = [
+	    {
+	        value: f1.votes,
+	        color:"#F7464A",
+	        highlight: "#FF5A5E",
+	        label: "Red"
+	    },
+	    {
+	        value: f2.votes,
+	        color: "#46BFBD",
+	        highlight: "#5AD3D1",
+	        label: "Green"
+	    },	
+	    {
+	        value: f3.votes,
+	        color: "#FDB45C",
+	        highlight: "#FFC870",
+	        label: "Yellow"
+	    }
+	];
+	var ctx = document.getElementById("myChart").getContext("2d");
+	var myDoughnutChart = new Chart(ctx).Doughnut(data);
+}
+makeChart();
 
-img1.src = imgL;
-img2.src = imgR;
+// console.log('imageL', imgL);
+
 
